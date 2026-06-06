54a6c4de7e429c335a027b0b5b2911e7f3e68fe1
Matvei Plokhov <mpplokho@gmail.com>
2026-06-06 14:28:38 +0200

    Centralize share intent and composer test tags

--- src/com/android/messaging/ui/shareintent/common/ShareSubjectChip.kt
+++ src/com/android/messaging/ui/shareintent/common/ShareSubjectChip.kt
@@ -6,9 +6,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import com.android.messaging.ui.common.components.composer.MessageSubjectChip
 
-internal const val SHARE_SUBJECT_CHIP_TEST_TAG = "share_subject_chip"
-internal const val SHARE_SUBJECT_CHIP_CLEAR_BUTTON_TEST_TAG = "share_subject_chip_clear_button"
-
 internal fun shareComposeSubjectSlot(
     subjectText: String,
     onClear: () -> Unit,
