0da8db5da776532adf4a8663a706e9054e8d2d89
Matvei Plokhov <mpplokho@gmail.com>
2026-06-10 22:04:47 +0200

    Add conversation list item components

---
+++ src/com/android/messaging/ui/conversationlist/redesign/ui/ConversationListTokens.kt
@@ -0,0 +1,18 @@
+package com.android.messaging.ui.conversationlist.redesign.ui
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+internal val ItemAvatarSize = 48.dp
+
+internal val ItemAvatarFallbackSize = 20.dp
+
+internal val ItemHeaderSpacing = 8.dp
+
+internal val ItemBadgeIconSize = 16.dp
+
+internal val ItemUnreadDotSize = 8.dp
+
+internal val ItemPreviewThumbnailSize = 44.dp
+
+internal val ItemPreviewThumbnailShape = RoundedCornerShape(size = 8.dp)
